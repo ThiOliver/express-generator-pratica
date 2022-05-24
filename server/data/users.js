f4512cc13f9b95b74006f86a18b09ae013e82130
Thiago Oliveira <thiago.oliver.siqueira@gmail.com>
2022-05-24 08:45:14 -0300

    percorrendo users na view users.js

---
+++ server/data/users.js
@@ -0,0 +1,36 @@
+const users = [ 
+    {
+    id: 1,
+    nome: "Fulano",
+    sobrenome: "Da siçva",
+    idade: 22,
+    email: "fulano@detal.com",
+    assinate: "true"
+},
+{
+    id: 2,
+    nome: "Beltrano",
+    sobrenome: "Da silva",
+    idade: 26,
+    email: "beltrano@dasilva.com",
+    assinate: "false"
+},
+{
+    id: 3,
+    nome: "Jorel",
+    sobrenome: "Da Cunha",
+    idade: 25,
+    email: "jorel@dacunha.com",
+    assinate: "true"
+},
+{
+    id: 4,
+    nome: "José",
+    sobrenome: "De Oliveira",
+    idade: 29,
+    email: "beltrano@dasilva.com",
+    assinate: "false"
+},
+]
+
+module.exports = users
