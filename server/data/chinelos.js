e4437fb62aab7aa69c2e1d71c623e981d68a48e7
Thiago Oliveira <thiago.oliver.siqueira@gmail.com>
2022-05-27 15:55:59 -0300

    feat: adicionando chinelos ao código

---
+++ server/data/chinelos.js
@@ -0,0 +1,34 @@
+const chinelos = [
+    {
+        id: 1,
+        marca:"Havaianas",
+        cor: "#f00",
+        ano: 1998,
+        exclusiva: false
+    },
+    {
+        id: 2,
+        marca:"Havaianas",
+        cor: "#000",
+        ano: 1998,
+        exclusiva: true
+    },
+    {
+        id: 3,
+        marca:"Raider",
+        cor: " #f00",
+        ano: 1998,
+        exclusiva: false
+    },
+    {
+        id: 4,
+        marca:"Raider",
+        cor: "#f0f",
+        ano: 1998,
+        exclusiva: true
+    },
+
+]
+
+
+module.exports = chinelos
